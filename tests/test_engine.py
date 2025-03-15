260a6e9d15d3b98fc30614c5bc128711b933a3a7
Yi-sir <969299937@qq.com>
2025-03-15 17:42:46 +0800

    update code

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -7,17 +7,17 @@ RAG_ENGINE_CONFIG_PATH = os.path.join(JSON_DIR, "config.json")
 RAG_LOGGER_CONFIG_PATH = os.path.join(JSON_DIR, "logging.json")
 
 sys.path.append(BASE_DIR)
+from app.utils.logger import setup_logging
+setup_logging(RAG_LOGGER_CONFIG_PATH)
 
 from app.engine.rag_engine import RAGEngine
 from app.engine.config import RAGConfig
-from app.utils.logger import setup_logging
 from app.models.generator.generator_api import GeneratorAPI
 from app.document_processing.database.database_faiss import DataBaseFaiss
 from app.document_processing.splitter.fixed_len_splitter import FixedLengthSplitter
 
 
 if __name__ == "__main__":
-    setup_logging(RAG_LOGGER_CONFIG_PATH)
 
     engine_config = RAGConfig.from_json(RAG_ENGINE_CONFIG_PATH)
     engine = RAGEngine(engine_config)
